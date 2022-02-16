b85d090438a99d104c675d2edd0d5cd858e0003d
Miles Murgaw <milesmurgaw@gmail.com>
2022-02-16 18:04:23 -0500

    feat: our first route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use dioxus::prelude::*;
+use dioxus::{
+    prelude::*,
+    router::{Route, Router},
+};
 
 fn main() {
     dioxus::web::launch(app);
@@ -6,6 +9,22 @@ fn main() {
 
 fn app(cx: Scope) -> Element {
     cx.render(rsx! {
-        p { "Hello, wasm!"}
+        Router {
+            p { "-- Dioxus Blog --" }
+            Route { to: "/", self::homepage {}}
+            Route { to: "", self::page_not_found {}}
+        }
+    })
+}
+
+fn homepage(cx: Scope) -> Element {
+    cx.render(rsx! {
+        p { "Welcome to Dioxus Blog!" }
+    })
+}
+
+fn page_not_found(cx: Scope) -> Element {
+    cx.render(rsx! {
+        p { "Oops! The page you are looking for doesn't exist!" }
     })
 }
